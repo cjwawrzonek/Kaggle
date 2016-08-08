2eefcba83fcf671848aea81594c0955152f62f41
Christian Wawrzonek <cjw@christians-MBP.fios-router.home>
2016-08-08 01:19:17 -0400

    Simple NN model

--- MNIST/NN/NN.py
+++ MNIST/NN/NN.py
@@ -3,42 +3,85 @@
 ##########################################################################################
 
 import copy
-import pickle
+import cPickle as pickle
 import random
 import math
 import sys
+import json
 
-from .utility import *
+import utility as util
 import numpy as np
 import model as md
 
 from sklearn.neighbors import KNeighborsClassifier
 
-def NNmodel(modelpath=None):
-	train = util.readData('data/train.csv')
+def NNmodel(model=None):
 
-	y = train[0:23999, 0]
-	x = train[0:23999, 1:]
+	print "Parsing data."
 
-	y2 = train[24000: ,0]
-	x2 = train[24000:, 1:]
+	modelpath = "models/{}".format(model)
 
-	print "Got here"
+	train = util.readData('../data/train.csv')
 
-	knbrs = KNeighborsClassifier(n_neighbors=3)
-	knbrs.fit(x, y)
+	## See if the data was pickled. If yes, fetch. If no, load and pickle
+	# try:
+	# 	train = pickle.load(open( "train.pkl", "rb" ))
+	# except:
+	# 	# this is potentially time intensive
+	# 	train = util.readData('data/train.csv')
 
-	print "Neighbors fit"
+	# 	# pickle it
+	# 	pickle.dump(train, open( "train.pkl", "wb" ))
 
-	y_pred = knbrs.predict(x2)
+	with open('{}/params.json'.format(modelpath)) as param_file:    
+		params = json.load(param_file)
 
-	print "Predictions made"
+	# If we're cross validating, not yet implemented
+	if params['cross_valid']:
+		pass
 
-	print y2[0:10]
-	print y_pred[0:10]
+	num_rows = len(train)
+
+	if params['testing']:
+		x = train[0:(len(train)/2), 1:]
+		y = train[0:(len(train)/2), 0]
+
+		x_test = train[(len(train)/2 + 1):, 1:]
+		y_test = train[(len(train)/2 + 1): ,0]
+
+		print "Finished parsing data. Fitting NN."
+
+		knbrs = KNeighborsClassifier(n_neighbors=params['num_neighbors'])
+		knbrs.fit(x, y)
+
+		print "Finished fitting. Running predictions."
+
+		y_pred = knbrs.predict(x_test)
+
+		print "Predictions done. Calculating results."
+
+		correct = 0
+		incorrect = 0
+		for i in range(len(y_pred)):
+			if y_pred[i] == y_test[i]:
+				correct += 1
+			else:
+				incorrect += 1
+
+		percent = float(correct) / (float(correct + incorrect))
+
+		print "Num correct = {}. Percent correct = {}".format(correct, percent)
+
+		return
 
 def main():
-    NNmodel()
+	try:
+		model = sys.argv[1]
+	except:
+		print 'No model parameters specified, using "m1" \nTo specify parameters, use "python NN [parameter file]"'
+		model = None
+
+	NNmodel(model)
 
 if __name__ == "__main__":
-    main()
+	main()
